44aff9a860deb2117eaecd4cbcc460471e0f5e22
skeptycal <skeptycal@gmail.com>
2019-06-24 19:00:05 -0500

    formatting

--- plist_xml.sh
+++ plist_xml.sh
@@ -7,10 +7,10 @@
 # Parameters:
 #
 #   $1: name of file to convert (no default - must exist and be writable)
-#   $2: format to convert to; in {xml1, binary, json} (default xml1)
+#   $2: format to convert to; in {xml1, binary1, json} (default xml1)
 #
 # @author    Michael Treanor  <skeptycal@gmail.com>
-# @copyright 2019 © Michael Treanor
+# @copyright 2019 ï¿½ Michael Treanor
 # @license   MIT <https://opensource.org/licenses/MIT>
 # @version   0.1.0
 # @link      http://www.github.com/skeptycal
@@ -21,102 +21,81 @@
 MAIN='\u001b[38;5;229m'
 WARN='\u001b[38;5;203m'
 BLUE='\u001b[38;5;38m'
-printf $MAIN
+WHITE='\u001b[37m'
+PURPLE='\u001b[38;5;93m'
 
-# test file config.plist - response should be
-#   config.plist: Apple binary property list
-# At least one parameter is required - it must be a valid filename
-[ $# -eq 0 ] && { printf $WARN"\n-> $0 Parameters required.\n\n"$MAIN"Usage: $0 SOURCE [FORMAT]\n\t- SOURCE MUST BE A VALID .PLIST FILE.\n\t- FORMAT IN {XML1, BINARY, JSON} (DEFAULT XML1)"; exit 1; }
+usage="""${MAIN}NAME${WHITE}
+    plist_xml - convert plist files
 
-[ -!f ] && { printf $WARN"\n-> $0 Parameters required.\n\n"$MAIN"Usage: $0 SOURCE [FORMAT]\n\t- SOURCE MUST BE A VALID .PLIST FILE.\n\t- FORMAT IN {XML1, BINARY, JSON} (DEFAULT XML1)"; exit 1; }
+${MAIN}SYNOPSIS${WHITE}
+    plist_xml SOURCE [FORMAT] [MINE]
 
+${MAIN}DESCRIPTION${WHITE}
+    Convert macOS plist file SOURCE to the optional FORMAT requested. The
+    default format is xml1. If the file is already in the requested format,
+    nothing is done. If the format requested is available, the file is
+    translated and a copy is written with a new extension that reflects the
+    new format.
 
+    ${MAIN}SOURCE${WHITE} - existing plist file
+    ${MAIN}FORMAT${WHITE} - optional format in (xml1 | binary1 | json)
+        (default is xml1)
+    ${MAIN}MINE${WHITE} - set to 'mine' to retain (non-sudo) ownership
+        of the new file (default is current owner)
+
+"""
 
 ###############################################################################
 # Activate Sudo Environment
 ###############################################################################
 
+current_user="$(whoami)"
 # Sudo Environment: (optional, uncomment BOTH 'sudo -v' and 'while true ...')
 # Get the administrator password upfront
 sudo -v
-
 # Keep-alive: update existing `sudo` time stamp until script has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
-
-# Function echoit($text)
-# echo text with bold formatting
-
-white='\u001b[37m'
-purple='\u001b[38;5;93m'
 
 ###############################################################################
 # Script Content
 ###############################################################################
 
-printf $purple"Plist_xml"$white
+# escape hatch ...
+function getout() {
+    [[ -f "$new_file" ]] && rm -f "$new_file"
+    printf "${WARN}${2} ${1}\n\n${WHITE}"
+    printf "$usage"
+    exit 1
+}
+
+########################
+# test parameters
+[[ -z "$1" ]] && getout "$1" "Required parameter missing. "
+[[ -f "$1" ]] || getout "$1" "File not found: "
+[[ -r "$1" ]] && file="$1" || getout "$1" "File not readable: "
+# set format option to default or $2
+[[ -z "$2" ]] && option='xml1' || option="$2"
+[[ "$3" == 'mine' ]] && mine='mine' || mine=''
+# test requested format
+[[ -n $(echo 'xml1 binary1 json' | xargs -n1 echo | grep -e "^${option}$") ]] && echo "Requested file format: ${option}" || getout "$option" "Requested option not available: "
+
+########################
+# check file format
+plutil -lint "$file"
+[[ "$?" ]] && printf "File linting successful. Original format is valid: ${file}\n" || getout "$file" "File format incompatible: "
+
+########################
+# All tests checkout ... perform conversion
+new_file="${file}.${option}"
+# backup original
+sudo cp -af "$file" "${file}.bak"
+# create working copy
+sudo cp -af "$file" "$new_file"
+# if $mine option ... make it mine
+[[ $mine ]] && sudo chown "$current_user" "$new_file"
+# convert
+sudo plutil -convert "$option" "$new_file"
+[[ "$?" ]] && printf "Conversion successful\n" || getout "$file" "Error converting to ${option} format : "
+[[ "$?" ]] && printf "New file created.\n" || getout "$file" "Error renaming file. New file uses original name: "
 printf '\n'
-printf 'Convert macOS .plist files from binary to xml format.'
-
-if [ -n $2 ]
-
-
-echo "$0: File '${file}' not found."
-
-if [ -a $1 ]
-  then
-    if [ - ]
-
-    # Backup Preferences folder to Preferences.bak
-    sudo cp -f $1 "$1.bak"
-    # Change ownership on the plist file
-    # sudo chown -v $(whoami):staff $temp_plist
-    # Convert binary --> xml
-    sudo plutil -convert xml1 $1
-    if [ -nz $? ]
-      echo "There was an error performing the conversion. (error code $?)"
-    else
-      echo "The file $1 was converted from binary to XML."
-else
-  echo "The file $1 does not exist in the current or given path."
-fi
-
-
-
-```
-
-  - *do your editing or processing ...*
-  - *test everything out ...*
-
-```BASH
-# convert back to default (xml --> binary)
-# this may not be necessary, but ... why not
-sudo plutil -convert binary1 com.apple.dock.plist
-
-# restore default ownership
-# also may be optional, but better safe ...
-sudo chown -v root:wheel com.apple.dock.plist
-```
-
-
-""" Usage: file [OPTION...] [FILE...]
-    Determine type of FILEs.
-    """
-
-
-
-
-
-
-
-
-# for file in ~/.dotfiles/.{path,bash_prompt_zsh,exports,aliases,functions,extra}; do
-# 	[ -r "$file" ] && [ -f "$file" ] && source "$file";
-# done;
-# unset file;
-
-
-###############################################################################
-# General Cleanup
-###############################################################################
-
-killall Dock
+printf "${MAIN}${file} was converted to ${option} format. New filename: ${new_file}\n\n"
